2e026e4f3e96a3d12ced2703219669175279b0a3
Ana C Sanchez <ana@anacsanchez.com>
2019-05-07 20:28:57 -0400

    added markdown package and styling to search results

--- client/components/SearchResult.js
+++ client/components/SearchResult.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Markdown from 'markdown-to-jsx';
 
 const SearchResult = ({ result }) => {
   const { author, selftext, title, subreddit, url, thumbnail } = result;
@@ -7,7 +8,7 @@ const SearchResult = ({ result }) => {
       <div className="result-sub">r/{subreddit}</div>
       <div className="result-author">{author}</div>
       <div className="result-title"><a href={url}>{title}</a></div>
-      <div className="result-content">{selftext}</div>
+      <div className="result-content"><Markdown>{selftext}</Markdown></div>
     </div>
   );
 };
