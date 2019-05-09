e8d623dd822b5594212c1d9e4ee5ef67b06fe4e3
Ana C Sanchez <ana@anacsanchez.com>
2019-05-09 12:50:21 -0400

    added links for subreddit, changed thread link to reddit permalink and url property to thumbnail when applicable, added default text for preview if spoiler post

--- client/components/ResultText.js
+++ client/components/ResultText.js
@@ -1,23 +1,24 @@
 import React, { useState } from 'react';
 import Markdown from 'markdown-to-jsx';
 
-const ResultText = ({ text }) => {
+const ResultText = ({ isSpoiler, text }) => {
   const [ isExpanded, setExpanded ] = useState(false);
 
   return (
     <div className="result-text-content" onClick={() => setExpanded(!isExpanded)} >
-      { isExpanded ?
-        <div className="result-text-container">
-          <div className="result-text-inner">
-            <Markdown>{text}</Markdown>
-          </div>
-        </div> :
-        <div className="result-text-preview-container">
-          <div className="result-text-preview">
-              <Markdown>{text.slice(0, 1000)}</Markdown>
-          </div>
+    {
+      isExpanded ?
+      <div className="result-text-container">
+        <div className="result-text-inner">
+          <Markdown>{text}</Markdown>
         </div>
-      }
+      </div> :
+      <div className="result-text-preview-container">
+        <div className="result-text-preview">
+          { isSpoiler ? <h2>SPOILERS - Click to Expand</h2> : <Markdown>{text.slice(0, 1000)}</Markdown> }
+        </div>
+      </div>
+    }
     </div>
   );
 };
